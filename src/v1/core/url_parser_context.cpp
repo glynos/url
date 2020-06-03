b8f71506611b9c4d4d683618250ace894bc1b3eb
Glyn Matthews <glyn.matthews@gmail.com>
2020-06-03 10:16:45 +0200

    Added more code points to the list that are forbidden (#111)

--- src/v1/core/url_parser_context.cpp
+++ src/v1/core/url_parser_context.cpp
@@ -27,7 +27,7 @@ auto contains(char byte, std::string_view view) noexcept {
 
 auto is_forbidden_host_point(std::string_view::value_type byte) noexcept {
   using namespace std::string_view_literals;
-  constexpr auto forbidden = "\0\t\n\r #%/:?@[\\]"sv;
+  constexpr auto forbidden = "\0\t\n\r #%/:<>?@[\\]^"sv;
   auto first = begin(forbidden), last = end(forbidden);
   return last != std::find(first, last, byte);
 }
